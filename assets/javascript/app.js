621e730b6daf99a12a11ce648a7ae22ddc0d905e
Andrew Gaurin <gaurin1107@gmail.com>
2018-10-21 13:36:15 -0500

    mostly collection functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 const LOCAL_STORAGE_VARS = {
   username: "username",
+  bggname: "bggname",
+  collection: "coll",
 };
 
 // https://boardgamegeek.com/wiki/page/BGG_XML_API2
@@ -46,7 +48,7 @@ let BGG_API = {
       });
   },
 
-  fetchUsersOwnedCollection(user) {
+  getUsersOwnedCollection(user) {
     BGG_API.config = {
       path: "/collection",
       params: {
@@ -78,10 +80,10 @@ let BGG_API = {
     BGG_API.config = {
       path: "/user",
       params: {
-        name: name,
-        buddies: 0,
-        guilds: 0,
-        domain: "boardgame"
+        name   : name,       // target
+        buddies: 0,          // ignore
+        guilds : 0,          // ignore
+        domain : "boardgame" // filter
       }
     };
     // #region Example Response
@@ -113,28 +115,34 @@ let BGG_API = {
     // let exampleRespObj_valid = { "$": {}, "items": { "$": { "termsofuse": "https://boardgamegeek.com/xmlapi/termsofuse" }, "item": { "$": { "type": "boardgame", "id": "68448" }, "thumbnail": "https://cf.geekdo-images.com/thumb/img/Grz-qM9xwxlvQGK7B-MiljtO9pQ=/fit-in/200x150/pic860217.jpg", "image": "https://cf.geekdo-images.com/original/img/3DP_RW5lTX0WrV67s8qi8CsiXoQ=/0x0/pic860217.jpg", "name": [{ "$": { "type": "primary", "sortindex": "1", "value": "7 Wonders" }, "_": "" }, { "$": { "type": "alternate", "sortindex": "1", "value": "7 csoda" }, "_": "" }, { "$": { "type": "alternate", "sortindex": "1", "value": "7 Cudów Świata" }, "_": "" }, { "$": { "type": "alternate", "sortindex": "1", "value": "7 divů světa" }, "_": "" }, { "$": { "type": "alternate", "sortindex": "1", "value": "7 чудес" }, "_": "" }, { "$": { "type": "alternate", "sortindex": "1", "value": "Τα 7 θαύματα του κόσμου" }, "_": "" }, { "$": { "type": "alternate", "sortindex": "1", "value": "七大奇蹟" }, "_": "" }, { "$": { "type": "alternate", "sortindex": "1", "value": "世界の七不思議" }, "_": "" }], "description": "You are the leader of one of the 7 great cities of the Ancient World. Gather resources, develop commercial routes, and affirm your military supremacy. Build your city and erect an architectural wonder which will transcend future times.&#10;&#10;7 Wonders lasts three ages. In each age, players receive seven cards from a particular deck, choose one of those cards, then pass the remainder to an adjacent player. Players reveal their cards simultaneously, paying resources if needed or collecting resources or interacting with other players in various ways. (Players have individual boards with special powers on which to organize their cards, and the boards are double-sided). Each player then chooses another card from the deck they were passed, and the process repeats until players have six cards in play from that age. After three ages, the game ends.&#10;&#10;In essence, 7 Wonders is a card development game. Some cards have immediate effects, while others provide bonuses or upgrades later in the game. Some cards provide discounts on future purchases. Some provide military strength to overpower your neighbors and others give nothing but victory points. Each card is played immediately after being drafted, so you'll know which cards your neighbor is receiving and how his choices might affect what you've already built up. Cards are passed left-right-left over the three ages, so you need to keep an eye on the neighbors in both directions.&#10;&#10;Though the box of earlier editions is listed as being for 3&ndash;7 players, there is an official 2-player variant included in the instructions.&#10;&#10;", "yearpublished": { "$": { "value": "2010" }, "_": "" }, "minplayers": { "$": { "value": "2" }, "_": "" }, "maxplayers": { "$": { "value": "7" }, "_": "" }, "poll": [{ "$": { "name": "suggested_numplayers", "title": "User Suggested Number of Players", "totalvotes": "1646" }, "results": [{ "$": { "numplayers": "1" }, "result": [{ "$": { "value": "Best", "numvotes": "4" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "11" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "922" }, "_": "" }] }, { "$": { "numplayers": "2" }, "result": [{ "$": { "value": "Best", "numvotes": "108" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "318" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "749" }, "_": "" }] }, { "$": { "numplayers": "3" }, "result": [{ "$": { "value": "Best", "numvotes": "358" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "840" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "146" }, "_": "" }] }, { "$": { "numplayers": "4" }, "result": [{ "$": { "value": "Best", "numvotes": "843" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "565" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "29" }, "_": "" }] }, { "$": { "numplayers": "5" }, "result": [{ "$": { "value": "Best", "numvotes": "693" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "661" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "42" }, "_": "" }] }, { "$": { "numplayers": "6" }, "result": [{ "$": { "value": "Best", "numvotes": "318" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "848" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "122" }, "_": "" }] }, { "$": { "numplayers": "7" }, "result": [{ "$": { "value": "Best", "numvotes": "289" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "773" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "210" }, "_": "" }] }, { "$": { "numplayers": "7+" }, "result": [{ "$": { "value": "Best", "numvotes": "26" }, "_": "" }, { "$": { "value": "Recommended", "numvotes": "67" }, "_": "" }, { "$": { "value": "Not Recommended", "numvotes": "620" }, "_": "" }] }] }, { "$": { "name": "suggested_playerage", "title": "User Suggested Player Age", "totalvotes": "366" }, "results": { "$": {}, "result": [{ "$": { "value": "2", "numvotes": "0" }, "_": "" }, { "$": { "value": "3", "numvotes": "0" }, "_": "" }, { "$": { "value": "4", "numvotes": "1" }, "_": "" }, { "$": { "value": "5", "numvotes": "0" }, "_": "" }, { "$": { "value": "6", "numvotes": "14" }, "_": "" }, { "$": { "value": "8", "numvotes": "99" }, "_": "" }, { "$": { "value": "10", "numvotes": "140" }, "_": "" }, { "$": { "value": "12", "numvotes": "90" }, "_": "" }, { "$": { "value": "14", "numvotes": "18" }, "_": "" }, { "$": { "value": "16", "numvotes": "3" }, "_": "" }, { "$": { "value": "18", "numvotes": "1" }, "_": "" }, { "$": { "value": "21 and up", "numvotes": "0" }, "_": "" }] } }, { "$": { "name": "language_dependence", "title": "Language Dependence", "totalvotes": "380" }, "results": { "$": {}, "result": [{ "$": { "level": "1", "value": "No necessary in-game text", "numvotes": "291" }, "_": "" }, { "$": { "level": "2", "value": "Some necessary text - easily memorized or small crib sheet", "numvotes": "84" }, "_": "" }, { "$": { "level": "3", "value": "Moderate in-game text - needs crib sheet or paste ups", "numvotes": "4" }, "_": "" }, { "$": { "level": "4", "value": "Extensive use of text - massive conversion needed to be playable", "numvotes": "1" }, "_": "" }, { "$": { "level": "5", "value": "Unplayable in another language", "numvotes": "0" }, "_": "" }] } }], "playingtime": { "$": { "value": "30" }, "_": "" }, "minplaytime": { "$": { "value": "30" }, "_": "" }, "maxplaytime": { "$": { "value": "30" }, "_": "" }, "minage": { "$": { "value": "10" }, "_": "" }, "link": [{ "$": { "type": "boardgamecategory", "id": "1050", "value": "Ancient" }, "_": "" }, { "$": { "type": "boardgamecategory", "id": "1002", "value": "Card Game" }, "_": "" }, { "$": { "type": "boardgamecategory", "id": "1029", "value": "City Building" }, "_": "" }, { "$": { "type": "boardgamecategory", "id": "1015", "value": "Civilization" }, "_": "" }, { "$": { "type": "boardgamemechanic", "id": "2041", "value": "Card Drafting" }, "_": "" }, { "$": { "type": "boardgamemechanic", "id": "2040", "value": "Hand Management" }, "_": "" }, { "$": { "type": "boardgamemechanic", "id": "2004", "value": "Set Collection" }, "_": "" }, { "$": { "type": "boardgamemechanic", "id": "2020", "value": "Simultaneous Action Selection" }, "_": "" }, { "$": { "type": "boardgamemechanic", "id": "2015", "value": "Variable Player Powers" }, "_": "" }, { "$": { "type": "boardgamefamily", "id": "17552", "value": "7 Wonders" }, "_": "" }, { "$": { "type": "boardgamefamily", "id": "27646", "value": "Tableau Building" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "247315", "value": "7 Wonders: Armada" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "154638", "value": "7 Wonders: Babel" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "110308", "value": "7 Wonders: Catan" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "111661", "value": "7 Wonders: Cities" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "92539", "value": "7 Wonders: Leaders" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "83445", "value": "7 Wonders: Manneken Pis" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "133993", "value": "7 Wonders: Wonder Pack" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "164649", "value": "Collection (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "140098", "value": "Empires (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "138187", "value": "Game Wonders (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "134849", "value": "Lost Wonders (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "131947", "value": "More Wonders... (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "132146", "value": "Myths (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "164648", "value": "Ruins (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgameexpansion", "id": "164647", "value": "Sailors (fan expansion for 7 Wonders)" }, "_": "" }, { "$": { "type": "boardgamedesigner", "id": "9714", "value": "Antoine Bauza" }, "_": "" }, { "$": { "type": "boardgameartist", "id": "9714", "value": "Antoine Bauza" }, "_": "" }, { "$": { "type": "boardgameartist", "id": "12016", "value": "Miguel Coimbra" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "4384", "value": "Repos Production" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "23043", "value": "ADC Blackfire Entertainment" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "157", "value": "Asmodee" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "15889", "value": "Asterion Press" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "15605", "value": "Galápagos Jogos" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "8820", "value": "Gém Klub Kft." }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "1391", "value": "Hobby Japan" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "6214", "value": "Kaissa Chess & Games" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "3218", "value": "Lautapelit.fi" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "9325", "value": "Lifestyle Boardgames Ltd" }, "_": "" }, { "$": { "type": "boardgamepublisher", "id": "7466", "value": "Rebel" }, "_": "" }] } } }
     // #endregion Example Response
 
-    return BGG_API._getAjax().then(function (thingObj) {
-      // console.log("thingobj: ", thingObj.items.item);
-      let mapped = thingObj.items.item.map(item => {
-        return {
-          description : item.description,
-          imageURL    : item.image      ,
-          thumbnailURL: item.thumbnail  ,
-          minage      : item.minage     .$.value,
-          minplayers  : item.minplayers .$.value,
-          maxplayer   : item.maxplayers .$.value,
-          minplaytime : item.minplaytime.$.value,
-          maxplaytime : item.maxplaytime.$.value,
-          playingtime : item.playingtime.$.value,
-          weight      : item.statistics .ratings.averageweight.$.value,
-        };
+    return BGG_API._getAjax()
+      .then(function (thingObj) {
+        let gameInfos = thingObj.items.item;
+        // console.log("thingobj: ", gameInfos);
+        if (!Array.isArray(gameInfos)){
+          gameInfos = [gameInfos]; // force to an array (of 1 item)
+        }
+        let mapped = gameInfos.map(item => {
+          return {
+            id          : item.$.id,
+            description : item.description,
+            imageURL    : item.image,
+            thumbnailURL: item.thumbnail,
+            minage      : item.minage.$.value,
+            minplayers  : item.minplayers.$.value,
+            maxplayer   : item.maxplayers.$.value,
+            minplaytime : item.minplaytime.$.value,
+            maxplaytime : item.maxplaytime.$.value,
+            playingtime : item.playingtime.$.value,
+            weight      : item.statistics.ratings.averageweight.$.value,
+          };
+        });
+        // console.log(mapped);
+        return mapped;
+      })
+      .catch(function (error) {
+        console.log("Error @ getGameInfoById", error);
       });
-      // console.log(mapped);
-      return mapped;
-    })
-    .catch(function(error) {
-      console.log("Error @ getGameInfoById", error);
-    });
   },
 
   searchForGameByName(name) {
@@ -149,20 +157,21 @@ let BGG_API = {
     // #region Example Response
     // #endregion Example Response
 
-    return BGG_API._getAjax().then(function(searchResults) {
-      // console.log(searchResults.items.item);
-      let ids="";
-      searchResults.items.item.forEach( item => {
-        ids += item.$.id+","
+    return BGG_API._getAjax()
+      .then(function(searchResults) {
+        // console.log(searchResults.items.item);
+        let ids="";
+        searchResults.items.item.forEach( item => {
+          ids += item.$.id+","
+        });
+        return ids;
+      })
+      .then(function(gameIDs) {
+        return BGG_API.getGameInfoById(gameIDs);
+      })
+      .catch(function(error) {
+        console.log("Error @ searchForGameByName", error);
       });
-      return ids;
-    })
-    .then(function(gameIDs) {
-      BGG_API.getGameInfoById(gameIDs);
-    })
-    .catch(function(error) {
-      console.log("Error @ searchForGameByName", error);
-    });
   }
 };
 
--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,58 +1,323 @@
-$(document).ready(() => {
-  "use strict";
-  console.log("LOGIN READY!");
+// $(document).ready(() => {
+"use strict";
+console.log("LOGIN READY!");
 
-  const JQ_IDs = {
-    signup_form: null,
-    signup_userName: null,
-    signup_bggUserName: null,
-    signup_submit: null
-  };
-  for (let id of Object.keys(JQ_IDs)) {
-    JQ_IDs[id] = $(`#${id}`);
-  }
+//#region DOM ELEMENTS
+const JQ_IDs = {
+  signup_form: null,
+  signup_userName: null,
+  signup_bggUserName: null,
+  signup_submit: null
+};
+for (let id of Object.keys(JQ_IDs)) {
+  JQ_IDs[id] = $(`#${id}`);
+}
+//#endregion DOM ELEMENTS
 
-  // Check Local Storage for username
-  let username = localStorage.getItem(LOCAL_STORAGE_VARS.username);
-  if (username){
-    console.log("GOT A NAME!", username);
-  }
-  else {
-    console.log("NEED A NAME");
-  }
+// Firebase References
+let database = getFirebaseDB();
+let userNamesRef = database.ref("/usernames");
+let    eventsRef = database.ref("/events");
 
+// Page-local variables
+let thisUsersRef = null;
+let username = "";
+let collection = [];
 
-  let database = getFirebaseDB();
-  var userNamesRef = database.ref("/usernames");
+function buildUpUserFB(username) {
+  console.log("building FB for user", username)
+  localStorage.setItem(LOCAL_STORAGE_VARS.username, username);
+  thisUsersRef = userNamesRef.child(username);
+  thisUsersRef.child("coll").on("child_added"  , listenForAddGame   );
+  thisUsersRef.child("coll").on("child_removed", listenForRemoveGame);
+}
 
-  // Login / Sign-Up
-  JQ_IDs.signup_form.submit(function(event) {
-    event.preventDefault();
+function tearDownUser() {
+  console.log("tearing down user");
+  thisUsersRef && thisUsersRef.child("coll").off("child_added", listenForAddGame);
+  thisUsersRef && thisUsersRef.child("coll").off("child_removed", listenForRemoveGame);
+  thisUsersRef = null;
+  localStorage.removeItem(LOCAL_STORAGE_VARS.username);
+  username = "";
+}
 
-    var username = JQ_IDs.signup_userName.val().trim();
-    if (!username) {
-      // TODO: validate/notify that we need the username
-      return false;
-    }
-
-    localStorage.setItem(LOCAL_STORAGE_VARS.username, username);
-
-    var bggUsername = JQ_IDs.signup_bggUserName.val().trim();
-
-    let myRef = userNamesRef.child(username);
-    myRef.set({ bgg: bggUsername });
-
-    // Clear form inputs
-    JQ_IDs.signup_userName   .val("");
-    JQ_IDs.signup_bggUserName.val("");
-
-    // if (bggUsername) {
-    //   BGG_API.checkUserName(bggUsername).then(nameObj => {
-    //     console.log(nameObj);
-    //   });
-    // }
-    return false;
+function checkFBforName(name) {
+  return userNamesRef.once("value").then(snap => {
+    return snap.child(name).exists();
   });
+}
 
-  // ADD
+//#region SIGN-IN/OUT
+// Login / Sign-up
+function signIn(name) {
+  console.log("Sign in/up as", name);
+  checkFBforName(name).then(doesExist => {
+    // Regardless if this is a previous user, we need the FB user node
+    buildUpUserFB(name);
+    if (doesExist) {
+      console.log("user already has account on FB");
+      // Firebase knows this name
+      populateCollectionFromFB(thisUsersRef);
+    } else {
+      // Firebase does not have this name
+      console.log("adding user to FB")
+      // Sign them up!
+      thisUsersRef.set({signUpAt: firebase.database.ServerValue.TIMESTAMP }); // give them a persistent data node to make sure they stay in the db
+      console.log("VISUAL: remove modal");
+    }
+  });
+}
+
+// Signout
+function signOut() {
+  // Remove name & collection from local storage
+  localStorage.clear();
+  tearDownUser();
+  collection = [];
+
+  // TODO: Clear elements on page
+  //  - profile image
+  //  - collection list
+  //  - events list
+}
+//#endregion SIGN-IN/OUT
+
+
+//#region BGG-LINK
+let BGGAccount = "";
+// Link to BG
+function linkToBGG() {
+  console.log("linking to BGG");
+  localStorage.setItem(LOCAL_STORAGE_VARS.bggname, BGGAccount);
+  thisUsersRef.update({ BGG: BGGAccount });
+  // TODO: what else?
+}
+// Resync with BGG
+function resyncWithBGG() {
+  console.log("resyncing with BGG");
+  localStorage.setItem(LOCAL_STORAGE_VARS.bggname, BGGAccount);
+  thisUsersRef.update({ BGG: BGGAccount });
+  // TODO: clear FB and local storage coll
+  // TODO: API call 
+}
+//#endregion BGG-LINK
+
+
+//#region GAMES COLLECTION
+
+//* Fetch Firebase Collection
+function populateCollectionFromFB(userRef) {
+  console.log("populating collection from firebase refkey=", userRef.key);
+  userRef.child("coll").once("value").then(function(dataSnap) {
+    const val = dataSnap.val();
+    if (!val){
+      console.log("user has no collection on FB");
+      console.log("VISUAL?: show where to add games, or link bgg")
+      return;
+    } else {
+      console.log("user has collection on FB", val);
+      console.log("show games collection on screen");
+      //TODO: show events on screen
+    }
+  });
+}
+
+//* Search BGG for game, to add to Collection
+function searchBGG(gameTitle) {
+  console.log("searching bgg for", gameTitle);
+  console.log("VISUAL: clear previous search results");
+  BGG_API.searchForGameByName(gameTitle).then( results => {
+    console.log("search results", results);
+    if (results.length === 0){
+      console.log("no results found");
+    }
+    else {
+      // TODO: visuals
+      console.log("VISUAL: add results to screen");
+      console.log("!!imp - add data attr for bggid");
+    }
+  });
+}
+
+//* Add to Game Collection
+function addGameToCollectionByID(id) {
+  console.log("add game to collection by id", id);
+
+  BGG_API.getGameInfoById(id).then(gameObjArray => {
+    const gameObj = gameObjArray[0];
+    // Add to Firebase 
+    thisUsersRef.child("coll").push(gameObj);
+    // Update local var 
+    collection.push(gameObj);
+    // Update local storage 
+    localStorage.setItem(LOCAL_STORAGE_VARS.collection, JSON.stringify(collection));
+  });
+}
+
+//* Manual Add to Collection
+function manualAddGameToCollection() {
+  //TODO: get data from FORM
+  console.log("manual add game to collection");
+
+  // const gameObj = gameObjArray[0];
+  // Add to Firebase 
+  let newGameRef = thisUsersRef.child("coll").push();
+  const gameObj = {
+    id: newGameRef.key, // use firebase's push() key for the id instead
+  };
+  newGameRef.set(gameObj);
+  // Update local var 
+  collection.push(gameObj);
+  // Update local storage 
+  localStorage.setItem(LOCAL_STORAGE_VARS.collection, JSON.stringify(collection));
+
+  // let testManualGameObj = {
+    // id          : item.$.id,
+    // description : item.description,
+    // thumbnailURL: item.thumbnail,
+    // minage      : item.minage.$.value,
+    // minplayers  : item.minplayers.$.value,
+    // maxplayer   : item.maxplayers.$.value,
+    // minplaytime : item.minplaytime.$.value,
+    // maxplaytime : item.maxplaytime.$.value,
+    // playingtime : item.playingtime.$.value,
+    // weight      : item.statistics.ratings.averageweight.$.value,
+  // };
+}
+
+//* Remove Game from Collection
+function removeGameFromCollectionByID(id) {
+  //
+  console.log("removing game from collection with id", id);
+  thisUsersRef.child('coll')
+    .orderByChild('id').equalTo(id)
+    .once("value").then(snap => { 
+      const val = snap.val();
+      // console.log("snap val", Object.keys(val)[0]);
+      thisUsersRef.child(`coll/${Object.keys(val)[0]}`).remove()
+      .then( remvRes => {
+        console.log("remove succeeded", remvRes);
+      });
+    });
+}
+
+//* Listeners
+function listenForAddGame(args) {
+  console.log("heard you wanted to add a game", args.val());
+}
+function listenForRemoveGame(args) {
+  console.log("heard you wanted to remove a game", args.val());
+}
+//#endregion GAMES COLLECTION
+
+
+//#region EVENTS
+//* Fetch Firebase Events
+function populateEventsFromFB(userRef) {
+  console.log("populating events from firebase refkey=", userRef.key);
+  userRef.child("events").once("value").then(function(dataSnap) {
+    const val = dataSnap.val();
+    if (!val){
+      console.log("user has no events on FB");
+      console.log("VISUAL?: show where to add/join events")
+      return;
+    } else {
+      console.log("user has events on FB", val);
+      console.log("show events on screen");
+      //TODO: show events on screen
+    }
+  });
+}
+
+//* Add to Events List
+function addEvent() {
+  //TODO: get data from FORM
+  console.log("add event to list");
+
+  // Add to Firebase 
+  let newEventRef = thisUsersRef.child("myEvents").push();
+  const eventUID = newEventRef.key.slice(-6).toUpperCASE();
+  newEventRef.remove();
+  const eventObj = {
+    created: firebase.database.ServerValue.TIMESTAMP,
+    /* // TODO: other form info
+    - expiration
+    - descrip 
+    */
+  };
+  eventsRef.child(eventUID).set(eventObj);
+  //? Update local var 
+  // collection.push(gameObj);
+  //? Update local storage 
+  // localStorage.setItem(LOCAL_STORAGE_VARS.collection, JSON.stringify(collection));
+
+  // let testManualGameObj = {
+    // id          : item.$.id,
+    // description : item.description,
+    // thumbnailURL: item.thumbnail,
+    // minage      : item.minage.$.value,
+    // minplayers  : item.minplayers.$.value,
+    // maxplayer   : item.maxplayers.$.value,
+    // minplaytime : item.minplaytime.$.value,
+    // maxplaytime : item.maxplaytime.$.value,
+    // playingtime : item.playingtime.$.value,
+    // weight      : item.statistics.ratings.averageweight.$.value,
+  // };
+}
+
+//TODO: edit event
+//TODO: click/goto event
+//TODO: join event
+//#endregion EVENTS
+
+
+//#region SUBMITS / CLICKS
+JQ_IDs.signup_form.submit(function(event) {
+  event.preventDefault();
+
+  var username_input = JQ_IDs.signup_userName.val().trim();
+  if (!username_input) {
+    //? TODO: validate/notify that we need the username
+    return false;
+  }
+
+  signIn(username_input);
+
+  // TODO: Clear form inputs
+  // JQ_IDs.signup_userName.val("");
+  // JQ_IDs.signup_bggUserName.val("");
+
+  return false;
 });
+//#endregion SUBMITS / CLICKS
+
+
+// #region START OF EXECUTION
+// Check Local Storage for username
+username = localStorage.getItem(LOCAL_STORAGE_VARS.username);
+if (username) {
+  console.log("local storage has a name", username);
+  checkFBforName(username).then(doesExist => {
+    console.log("user existence in FB? ", doesExist);
+    if (doesExist) {
+      // Firebase knows this name
+      buildUpUserFB(username);
+      populateCollectionFromFB(thisUsersRef);
+      populateEventsFromFB(thisUsersRef);
+    } else {
+      // Firebase does not have this name, local storage is invalid
+      tearDownUser();
+    }
+  });
+}
+if (!username) { // using if (instead of else) in order to also catch invalid localstorage from above
+  console.log("local storage has no name");
+  // TODO: localStorage.clear(); // clear other local storage? this correct, necessary to do?
+  // TODO: Show modal
+  console.log("show the login/signup modal!");
+}
+
+// TODO: bggUser  = localStorage.getItem(LOCAL_STORAGE_VARS.bggname );
+//#endregion START OF EXECUTION
+
+// });
