92bfd3845425e6f0e20e7497a6ae25f396dfd705
NiRuaG <gaurin1107@gmail.com>
2018-10-24 11:42:45 -0500

    switch alert to console log

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -485,7 +485,7 @@ function codeAddress() {
           position: results[0].geometry.location
       });
     } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+      console.log('Geocode was not successful for the following reason: ' + status);
     }
   });
 }
