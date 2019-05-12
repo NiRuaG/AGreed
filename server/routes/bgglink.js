285b2c65928bdf01a600163059e90df27f8ffdfd
NiRuaG <ajgaurin@gmail.com>
2019-05-12 22:09:24 +0000

    initial bgg route

--- server/routes/bgglink.js
+++ server/routes/bgglink.js
@@ -14,13 +14,13 @@ router.route('/')
 
   //* GET
   .get((req, res) => {
-
+    res.status(500).json({message: 'not implemented'});
   })
 
   //* POST
   .post((req, res) => {
     console.log(req.body);
-    res.status(200).json(req.body);
+    res.status(200).json(req.body);userXMLObj
   })
 
   ; // router.route('/')
