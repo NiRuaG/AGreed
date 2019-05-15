4a51aa6cbe82e15cdc60261aed46ebca11d82f8f
NiRuaG <ajgaurin@gmail.com>
2019-05-15 04:10:04 +0000

    test with ramda/path

--- parseTest.js
+++ parseTest.js
@@ -1,7 +1,34 @@
 const { parseString } = require('xml2js');
+const { path } = require('ramda');
 
-require('fs').readFile('./userdata.xml', function (err, data) {
-    parseString(data, function (err, result) {
-        console.dir(result);
-    });
-});
+const afterParse = async (err, result) => {
+  if (err) return;
+  // console.dir(result);
+
+    const id =
+      Promise.resolve(result)
+        .then(path(['user', '$', 'id']))
+
+        .then(id => {
+          // console.log("id:\n", id);
+          // console.log(`[${id}]`);
+
+          if (id) { return { id }; } // wrap id in an object
+
+          throw new Error("path to user's id = empty or DNE");
+        })
+
+        .catch(error => {
+          console.log("Error @ checkUserName", error);
+        });
+
+    console.log(await id);
+}
+
+const afterFileRead = (err, data) => {
+  if (!err) {
+    parseString(data, afterParse);
+  }
+}
+
+require('fs').readFile('./userdata.xml', afterFileRead);
